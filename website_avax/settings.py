cc844062019ef2f4b3df731f7eb98df0eedec342
aleksandrmerkulov <user.aleksandrmerkylov@ya.ru>
2022-12-06 20:27:43 +0400

    commit

--- website_avax/settings.py
+++ website_avax/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-40_td615$gpyhsh(0!#3$ahrbrma5-r%gdy)v+wf%5te--f0yb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
