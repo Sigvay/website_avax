3a960cbdfd35360eec97ed22c3fa6534cdd5f84d
aleksandrmerkulov <user.aleksandrmerkylov@ya.ru>
2022-12-14 20:57:06 +0400

    commit

--- orders/admin.py
+++ orders/admin.py
@@ -5,4 +5,4 @@ from orders.models import OrderEmirate
 
 @admin.register(OrderEmirate)
 class AdminOrderEmirate(admin.ModelAdmin):
-    list_display = ['engine', 'count', 'payment', 'completed']
+    list_display = ['engine', 'count', 'date_add', 'payment', 'completed']
