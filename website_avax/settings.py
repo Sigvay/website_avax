1ac28872ededa0eff95b9fa8826f3c72d651bec3
aleksandrmerkulov <user.aleksandrmerkylov@ya.ru>
2023-03-27 22:44:44 +0300

    commit

--- website_avax/settings.py
+++ website_avax/settings.py
@@ -22,10 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-40_td615$gpyhsh(0!#3$ahrbrma5-r%gdy)v+wf%5te--f0yb'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -150,4 +149,3 @@ EXCEL_DIR = os.path.join(BASE_DIR, 'minsk/file/excel')
 XML_DIR = os.path.join(BASE_DIR, 'minsk/file/xml')
 EXCEL_BASE_IMPORT = os.path.join(EXCEL_DIR, 'import.xlsx')
 EXCEL_BASE_EXPORT = os.path.join(EXCEL_DIR, 'export.xlsx')
-
