5635b6227830b96eaac199aaf456e0872473b9e0
DonMerkulio <user.aleksandrmerkylov@ya.ru>
2023-06-19 15:53:44 +0300

    commit

--- orders/admin.py
+++ orders/admin.py
@@ -3,6 +3,6 @@ from django.contrib import admin
 from orders.models import OrderEmirate
 
 
-@admin.register(OrderEmirate)
-class AdminOrderEmirate(admin.ModelAdmin):
-    list_display = ['engine', 'count', 'date_add', 'payment', 'completed']
+# @admin.register(OrderEmirate)
+# class AdminOrderEmirate(admin.ModelAdmin):
+#     list_display = ['engine', 'count', 'date_add', 'payment', 'completed']
