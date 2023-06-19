382416036fc332c2b98a21e48411eae0f5a4b06a
DonMerkulio <user.aleksandrmerkylov@ya.ru>
2023-06-19 15:52:22 +0300

    commit

--- cars/admin.py
+++ cars/admin.py
@@ -2,31 +2,31 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(Mark)
-class AdminMark(admin.ModelAdmin):
-    list_display = ['name']
-
-
-@admin.register(Model)
-class AdminModel(admin.ModelAdmin):
-    list_display = ['mark', 'model']
-
-
-@admin.register(Engines)
-class AdminModel(admin.ModelAdmin):
-    list_display = ['name', 'fuel', 'volume_engine', 'type_engine']
-
-
-@admin.register(FuelEngine)
-class AdminFuelEngine(admin.ModelAdmin):
-    list_display = ['name']
-
-
-@admin.register(Transmissions)
-class AdminTransmissions(admin.ModelAdmin):
-    list_display = ['name']
-
-
-@admin.register(DescriptionEngnine)
-class AdminDescriptionEngnine(admin.ModelAdmin):
-    list_display = ['description']
+# @admin.register(Mark)
+# class AdminMark(admin.ModelAdmin):
+#     list_display = ['name']
+#
+#
+# @admin.register(Model)
+# class AdminModel(admin.ModelAdmin):
+#     list_display = ['mark', 'model']
+#
+#
+# @admin.register(Engines)
+# class AdminModel(admin.ModelAdmin):
+#     list_display = ['name', 'fuel', 'volume_engine', 'type_engine']
+#
+#
+# @admin.register(FuelEngine)
+# class AdminFuelEngine(admin.ModelAdmin):
+#     list_display = ['name']
+#
+#
+# @admin.register(Transmissions)
+# class AdminTransmissions(admin.ModelAdmin):
+#     list_display = ['name']
+#
+#
+# @admin.register(DescriptionEngnine)
+# class AdminDescriptionEngnine(admin.ModelAdmin):
+#     list_display = ['description']
--- emirate/admin.py
+++ emirate/admin.py
@@ -2,25 +2,25 @@ from django.contrib import admin
 from .models import *
 
 
-@admin.register(Sellers)
-class AdminSallers(admin.ModelAdmin):
-    list_display = ['name', 'geoposition', 'phone', 'description']
-
-
-@admin.register(Container)
-class AdminContainer(admin.ModelAdmin):
-    list_display = ['country', 'name', 'moving_warehouse', 'loading_cost',
-                   'transportation_costs', 'customs_clearance', 'move',
-                   'arrived']
-
-
-@admin.register(PurchasedEngines)
-class AdminPurchase(admin.ModelAdmin):
-    list_display = ['container', 'number', 'mark', 'model',
-                   'year', 'engine_mark', 'engine_number', 'transmission', 'description', 'price', 'warehouse']
-
-
-@admin.register(EnginesInStock)
-class AdminEnginesInStock(admin.ModelAdmin):
-    list_display = ['mark', 'model', 'year', 'engine_mark',
-                    'transmission', 'description', 'price']
+# @admin.register(Sellers)
+# class AdminSallers(admin.ModelAdmin):
+#     list_display = ['name', 'geoposition', 'phone', 'description']
+#
+#
+# @admin.register(Container)
+# class AdminContainer(admin.ModelAdmin):
+#     list_display = ['country', 'name', 'moving_warehouse', 'loading_cost',
+#                    'transportation_costs', 'customs_clearance', 'move',
+#                    'arrived']
+#
+#
+# @admin.register(PurchasedEngines)
+# class AdminPurchase(admin.ModelAdmin):
+#     list_display = ['container', 'number', 'mark', 'model',
+#                    'year', 'engine_mark', 'engine_number', 'transmission', 'description', 'price', 'warehouse']
+#
+#
+# @admin.register(EnginesInStock)
+# class AdminEnginesInStock(admin.ModelAdmin):
+#     list_display = ['mark', 'model', 'year', 'engine_mark',
+#                     'transmission', 'description', 'price']
